57b41e5d4b6cfbfa7b9eaad321a0fe6d41fd369e
Frasydi <Frasydi137@Gmail.com>
2023-01-09 03:02:59 +0800

    fix About

--- src/router/about/index.tsx
+++ src/router/about/index.tsx
@@ -2,8 +2,9 @@ export default function About() {
     return( 
         <>
             <h1 style={{textAlign:"center"}}>Tentang</h1>
-            <div>
-
+            <div style={{display:"grid",placeItems:"center"}}>
+                <h2>Web ini dibuat oleh Ahmad Sydiq</h2>
+                <h2><a href="https://github.com/Frasydi">Github</a></h2>
             </div>
         </>
     )
