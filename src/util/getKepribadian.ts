f01204ef427b2d8b7aeb75b4c861740d14f19478
Frasydi <Frasydi137@Gmail.com>
2023-01-09 02:59:34 +0800

    fix kepribadian

--- src/util/getKepribadian.ts
+++ src/util/getKepribadian.ts
@@ -53,70 +53,70 @@ function getKepribadianTentang(kepribadian: string): {
       return {
         title: "Composer",
         tentang:
-          "orang yang kreatif, artistik, dan peka terhadap lingkungan sekitarnya. Mereka cenderung introvert, lebih menggunakan perasaan dan pengamatan mereka dalam memahami situasi dan orang, dan lebih suka mengikuti aliran daripada terikat pada rencana atau aturan yang sudah ditentukan. Mereka juga dianggap sebagai orang yang sensitif, emosional, dan menghargai kebebasan.",
+          "orang yang kreatif, artistik, dan peka terhadap lingkungan sekitarnya. Anda cenderung introvert, lebih menggunakan perasaan dan pengamatan Anda dalam memahami situasi dan orang, dan lebih suka mengikuti aliran daripada terikat pada rencana atau aturan yang sudah ditentukan. Anda juga dianggap sebagai orang yang sensitif, emosional, dan menghargai kebebasan.",
         kepribadian: kepribadian,
       };
     case "INFP":
       return {
         title: "Craftsman",
         tentang:
-          "orang yang idealis, kreatif, dan empatik. Mereka cenderung introvert, menggunakan intuisi dan perasaan mereka dalam memahami orang dan situasi, dan lebih suka mengikuti aliran daripada terikat pada rencana atau aturan yang sudah ditentukan. Mereka juga dianggap sebagai orang yang sensitif, emosional, dan memiliki prinsip-prinsip yang kuat.",
+          "orang yang idealis, kreatif, dan empatik. Anda cenderung introvert, menggunakan intuisi dan perasaan Anda dalam memahami orang dan situasi, dan lebih suka mengikuti aliran daripada terikat pada rencana atau aturan yang sudah ditentukan. Anda juga dianggap sebagai orang yang sensitif, emosional, dan memiliki prinsip-prinsip yang kuat.",
         kepribadian: kepribadian,
       };
     case "INTP":
       return {
         title: "Logician",
         tentang:
-          "orang yang cenderung introvert, berfikir secara konseptual dan abstrak, dan senang mengeksplorasi ide-ide baru dan konsep-konsep yang tidak terbatas oleh konvensi-konvensi yang ada. Mereka juga cenderung memiliki kemampuan analitis yang tajam dan suka mengeksplorasi masalah-masalah yang kompleks. Namun, mereka biasanya kurang tertarik pada aspek-aspek praktis dari kehidupan sehari-hari dan mungkin kurang terampil dalam mengelola tugas-tugas yang terorganisir dengan baik.",
+          "orang yang cenderung introvert, berfikir secara konseptual dan abstrak, dan senang mengeksplorasi ide-ide baru dan konsep-konsep yang tidak terbatas oleh konvensi-konvensi yang ada. Anda juga cenderung memiliki kemampuan analitis yang tajam dan suka mengeksplorasi masalah-masalah yang kompleks. Namun, Anda biasanya kurang tertarik pada aspek-aspek praktis dari kehidupan sehari-hari dan mungkin kurang terampil dalam mengelola tugas-tugas yang terorganisir dengan baik.",
         kepribadian: kepribadian,
       };
     case "ESTJ" : 
       return {
         title : "Supervisor",
         kepribadian,
-        tentang : "orang yang cenderung ekstravert, mengutamakan fakta-fakta yang terperinci dan nyata, dan senang mengatur dan mengorganisir orang lain. Mereka cenderung memiliki kemampuan pemecahan masalah yang praktis dan terampil dalam mengelola proyek-proyek yang terstruktur dengan baik. Namun, mereka mungkin kurang tertarik pada ide-ide abstrak atau konsep-konsep yang tidak terkait dengan dunia nyata."
+        tentang : "orang yang cenderung ekstravert, mengutamakan fakta-fakta yang terperinci dan nyata, dan senang mengatur dan mengorganisir orang lain. Anda cenderung memiliki kemampuan pemecahan masalah yang praktis dan terampil dalam mengelola proyek-proyek yang terstruktur dengan baik. Namun, Anda mungkin kurang tertarik pada ide-ide abstrak atau konsep-konsep yang tidak terkait dengan dunia nyata."
       }
     case "ESFJ" : 
       return {
         title : "Provider",
         kepribadian,
-        tentang : "orang yang cenderung ekstravert, mengutamakan emosi dan koneksi interpersonal, dan senang membantu orang lain. Mereka cenderung tertarik pada hal-hal praktis yang terkait dengan kehidupan sehari-hari dan suka bekerja dalam lingkungan yang terorganisir dengan baik. Namun, mereka mungkin kurang tertarik pada ide-ide abstrak atau konsep-konsep yang tidak terkait dengan dunia nyata."
+        tentang : "orang yang cenderung ekstravert, mengutamakan emosi dan koneksi interpersonal, dan senang membantu orang lain. Anda cenderung tertarik pada hal-hal praktis yang terkait dengan kehidupan sehari-hari dan suka bekerja dalam lingkungan yang terorganisir dengan baik. Namun, Anda mungkin kurang tertarik pada ide-ide abstrak atau konsep-konsep yang tidak terkait dengan dunia nyata."
       }
     case "ENFJ" : 
       return {
         title : "Mentor",
         kepribadian,
-        tentang : "orang yang cenderung ekstravert, mengutamakan emosi dan koneksi interpersonal, dan senang membantu orang lain mencapai potensi terbaik mereka. Mereka cenderung memiliki kemampuan komunikasi yang kuat dan dapat dengan mudah memahami perspektif orang lain. Mereka juga tertarik pada ide-ide abstrak dan konsep-konsep yang lebih luas, dan sering memiliki visi yang jelas tentang bagaimana dunia seharusnya berjalan. Namun, mereka mungkin kurang terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan mungkin mengalami kesulitan dalam mengambil keputusan yang sulit."
+        tentang : "orang yang cenderung ekstravert, mengutamakan emosi dan koneksi interpersonal, dan senang membantu orang lain mencapai potensi terbaik Anda. Anda cenderung memiliki kemampuan komunikasi yang kuat dan dapat dengan mudah memahami perspektif orang lain. Anda juga tertarik pada ide-ide abstrak dan konsep-konsep yang lebih luas, dan sering memiliki visi yang jelas tentang bagaimana dunia seharusnya berjalan. Namun, Anda mungkin kurang terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan mungkin mengalami kesulitan dalam mengambil keputusan yang sulit."
       }
     case "ENTJ" : 
       return {
         title : "Commander",
         kepribadian, 
-        tentang : "orang yang cenderung ekstravert, berfikir secara konseptual dan abstrak, dan memiliki kemampuan pemecahan masalah yang tajam. Mereka cenderung terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan memiliki kemampuan untuk mempengaruhi orang lain dengan mudah. Mereka juga sering memiliki visi yang jelas tentang bagaimana dunia seharusnya berjalan dan senang mengambil inisiatif dalam mencapai tujuan-tujuan mereka. Namun, mereka mungkin kurang memperhatikan perasaan atau kebutuhan orang lain dan mungkin kurang terampil dalam mengelola hubungan interpersonal."
+        tentang : "orang yang cenderung ekstravert, berfikir secara konseptual dan abstrak, dan memiliki kemampuan pemecahan masalah yang tajam. Anda cenderung terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan memiliki kemampuan untuk mempengaruhi orang lain dengan mudah. Anda juga sering memiliki visi yang jelas tentang bagaimana dunia seharusnya berjalan dan senang mengambil inisiatif dalam mencapai tujuan-tujuan Anda. Namun, Anda mungkin kurang memperhatikan perasaan atau kebutuhan orang lain dan mungkin kurang terampil dalam mengelola hubungan interpersonal."
       }
     case "ESTP" : 
       return {
         title : "Dynamo",
         kepribadian,
-        tentang : "orang yang cenderung ekstravert, mengutamakan fakta-fakta yang terperinci dan nyata, dan senang mengeksplorasi dunia di sekitar mereka dengan cara yang tidak terbatas oleh aturan atau konvensi. Mereka cenderung terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan memiliki kemampuan untuk mengambil inisiatif dan mengambil keputusan dengan cepat. Namun, mereka mungkin kurang tertarik pada ide-ide abstrak atau konsep-konsep yang tidak terkait dengan dunia nyata dan mungkin kurang terampil dalam mengelola hubungan interpersonal."
+        tentang : "orang yang cenderung ekstravert, mengutamakan fakta-fakta yang terperinci dan nyata, dan senang mengeksplorasi dunia di sekitar Anda dengan cara yang tidak terbatas oleh aturan atau konvensi. Anda cenderung terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan memiliki kemampuan untuk mengambil inisiatif dan mengambil keputusan dengan cepat. Namun, Anda mungkin kurang tertarik pada ide-ide abstrak atau konsep-konsep yang tidak terkait dengan dunia nyata dan mungkin kurang terampil dalam mengelola hubungan interpersonal."
       }
     case "ESFP" : 
       return {
         title : "Performer",
         kepribadian,
-        tentang : "orang yang cenderung ekstravert, mengutamakan emosi dan koneksi interpersonal, dan senang mengeksplorasi dunia di sekitar mereka dengan cara yang tidak terbatas oleh aturan atau konvensi. Mereka cenderung terampil dalam mengelola hubungan interpersonal dan memiliki kemampuan untuk menghibur orang lain dengan mudah. Namun, mereka mungkin kurang tertarik pada ide-ide abstrak atau konsep-konsep yang tidak terkait dengan dunia nyata dan mungkin kurang terampil dalam mengelola proyek-proyek yang terstruktur dengan baik."
+        tentang : "orang yang cenderung ekstravert, mengutamakan emosi dan koneksi interpersonal, dan senang mengeksplorasi dunia di sekitar Anda dengan cara yang tidak terbatas oleh aturan atau konvensi. Anda cenderung terampil dalam mengelola hubungan interpersonal dan memiliki kemampuan untuk menghibur orang lain dengan mudah. Namun, Anda mungkin kurang tertarik pada ide-ide abstrak atau konsep-konsep yang tidak terkait dengan dunia nyata dan mungkin kurang terampil dalam mengelola proyek-proyek yang terstruktur dengan baik."
       }
     case "ENFP" : 
       return {
         title : "Champion",
         kepribadian, 
-        tentang : "orang yang cenderung ekstravert, mengutamakan emosi dan koneksi interpersonal, dan senang mengeksplorasi ide-ide baru dan konsep-konsep yang tidak terbatas oleh konvensi-konvensi yang ada. Mereka cenderung memiliki kemampuan komunikasi yang kuat dan dapat dengan mudah memahami perspektif orang lain. Mereka juga tertarik pada ide-ide abstrak dan konsep-konsep yang lebih luas, dan sering memiliki visi yang jelas tentang bagaimana dunia seharusnya berjalan. Namun, mereka mungkin kurang terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan mungkin mengalami kesulitan dalam mengambil keputusan yang sulit."
+        tentang : "orang yang cenderung ekstravert, mengutamakan emosi dan koneksi interpersonal, dan senang mengeksplorasi ide-ide baru dan konsep-konsep yang tidak terbatas oleh konvensi-konvensi yang ada. Anda cenderung memiliki kemampuan komunikasi yang kuat dan dapat dengan mudah memahami perspektif orang lain. Anda juga tertarik pada ide-ide abstrak dan konsep-konsep yang lebih luas, dan sering memiliki visi yang jelas tentang bagaimana dunia seharusnya berjalan. Namun, Anda mungkin kurang terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan mungkin mengalami kesulitan dalam mengambil keputusan yang sulit."
       }
     case "ENTP" : 
       return {
         title : "Inventor",
         kepribadian,
-        tentang : "orang yang cenderung ekstravert, berfikir secara konseptual dan abstrak, dan senang mengeksplorasi ide-ide baru dan konsep-konsep yang tidak terbatas oleh konvensi-konvensi yang ada. Mereka cenderung memiliki kemampuan analitis yang tajam dan suka mengeksplorasi masalah-masalah yang kompleks. Mereka juga cenderung terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan memiliki kemampuan untuk mempengaruhi orang lain dengan mudah. Namun, mereka mungkin kurang tertarik pada aspek-aspek praktis dari kehidupan sehari-hari dan mungkin kurang terampil dalam mengelola hubungan interpersonal."
+        tentang : "orang yang cenderung ekstravert, berfikir secara konseptual dan abstrak, dan senang mengeksplorasi ide-ide baru dan konsep-konsep yang tidak terbatas oleh konvensi-konvensi yang ada. Anda cenderung memiliki kemampuan analitis yang tajam dan suka mengeksplorasi masalah-masalah yang kompleks. Anda juga cenderung terampil dalam mengelola proyek-proyek yang terstruktur dengan baik dan memiliki kemampuan untuk mempengaruhi orang lain dengan mudah. Namun, Anda mungkin kurang tertarik pada aspek-aspek praktis dari kehidupan sehari-hari dan mungkin kurang terampil dalam mengelola hubungan interpersonal."
       }
   }
   return {
